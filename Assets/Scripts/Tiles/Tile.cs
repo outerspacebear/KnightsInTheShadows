0be39e72a642f355882084cf4f82c9a5d9be9005
Viraj <virjune@gmail.com>
2020-06-11 15:23:07 +0100

    Added a basic road tile prefab, and hover detection

---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!tileHighlighter)
+        {
+            tileHighlighter = GameObject.Find("TileHighlighter");
+            tileHighlighterOriginalPosition = tileHighlighter.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        if(tileHighlighter)
+        {
+            tileHighlighter.transform.position = gameObject.transform.position;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if(tileHighlighter)
+        {
+            tileHighlighter.transform.position = tileHighlighterOriginalPosition;
+        }
+    }
+
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private bool canMoveOver;
+    [SerializeField]
+    private int movementCost;
+
+    private static GameObject tileHighlighter;
+    private static Vector3 tileHighlighterOriginalPosition;
+}
