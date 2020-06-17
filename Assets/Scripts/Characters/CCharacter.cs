193fe84fa6c5d57d74f5b9d568aa6fbf02ce1dde
Viraj <virjune@gmail.com>
2020-06-17 18:00:40 +0100

    Added basic Character and Team scripts

---
+++ Assets/Scripts/Characters/CCharacter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!startingTile)
+        {
+            Debug.LogError("No starting tile specified for character " + gameObject.name);
+            return;
+        }
+
+        gameObject.transform.position = startingTile.gameObject.transform.position;
+        occupyingTile = startingTile;
+
+        currentActionPoints = baseActionPoints;
+
+        if(!(playerHighlilghter = GameObject.Find("PlayerHighlighter")))
+        {
+            Debug.LogError("Coudln't find the player highlighter!");
+            return;
+        }
+        playerHighlighterDefaultPosition = playerHighlilghter.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Character " + gameObject.name + "clicked on!");
+        CharacterClickedOnEvent.Get().Invoke(this);
+    }
+
+    public void OnSelected()
+    {
+        if(playerHighlilghter)
+        {
+            playerHighlilghter.transform.position = gameObject.transform.position;
+        }
+    }
+
+    public void OnDeselected()
+    {
+        if(playerHighlilghter)
+        {
+            playerHighlilghter.transform.position = playerHighlighterDefaultPosition;
+        }
+    }
+
+    public int currentActionPoints { get; set; }
+    public CTile occupyingTile { get; set; }
+
+    [SerializeField]
+    private int movementPerAction;
+    [SerializeField]
+    private int baseActionPoints;
+    [SerializeField]
+    private CTile startingTile;
+
+    static GameObject playerHighlilghter;
+    static Vector3 playerHighlighterDefaultPosition;
+}
---
+++ Assets/Scripts/Characters/TeamManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!currentlySelectedCharacter)
+        {
+            TrySelectNextAvailableCharacter();
+        }
+
+        CharacterClickedOnEvent.Get().AddListener(OnCharacterClickedOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!currentlySelectedCharacter)
+        {
+            if (!TrySelectNextAvailableCharacter())
+            {
+                Debug.Log("All characters on this team have taken their turn");
+            }
+        }
+    }
+
+    bool HaveAllCharactersEndedTurn()
+    {
+        foreach(var character in characters)
+        {
+            if(character.currentActionPoints > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void OnCharacterClickedOn(CCharacter character)
+    {
+        if(character.currentActionPoints > 0)
+        {
+            SelectCharacter(character);
+        }
+    }
+
+    bool TrySelectNextAvailableCharacter()
+    {
+        foreach (var character in characters)
+        {
+            if (character.currentActionPoints > 0)
+            {
+                SelectCharacter(character);
+                return true;
+            }
+        }
+
+        currentlySelectedCharacter = null;
+        return false;
+    }
+
+    void SelectCharacter(CCharacter character)
+    {
+        if(currentlySelectedCharacter)
+        {
+            currentlySelectedCharacter.OnDeselected();
+        }
+
+        currentlySelectedCharacter = character;
+        currentlySelectedCharacter.OnSelected();
+
+        Debug.Log("Character " + currentlySelectedCharacter.name + " selected!");
+    }
+
+    [SerializeField]
+    List<CCharacter> characters;
+    CCharacter currentlySelectedCharacter;
+}
