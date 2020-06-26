eb6be5e9dd7f5333b942492f25cc9db39e57a5cf
Viraj <virjune@gmail.com>
2020-06-26 16:39:07 +0100

    Added sound settings

---
+++ Assets/Scripts/Main Menu/PlayerPrefsFloatSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class PlayerPrefsFloatSlider : MonoBehaviour
+{
+    public void OnSliderValueChanged()
+    {
+        PlayerPrefs.SetFloat(playerPrefName, slider.value);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerPrefName == null || playerPrefName == "")
+        {
+            Debug.LogError("No playerPrefs name assigned to toggler!");
+            Destroy(this);
+        }
+        slider = GetComponent<Slider>();
+
+        slider.value = PlayerPrefs.GetFloat(playerPrefName, 0);
+    }
+
+    [SerializeField]
+    string playerPrefName;
+    Slider slider;
+}
