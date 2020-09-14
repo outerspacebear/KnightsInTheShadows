52a0a5ffb4bc18bc9e9e73d8b1284cb443e7e3ff
Viraj <virjune@gmail.com>
2020-09-14 12:44:08 +0100

    Basic pause menu

--- Assets/Scripts/Level/LevelEvents.cs
+++ Assets/Scripts/Level/LevelEvents.cs
@@ -5,11 +5,15 @@ using UnityEngine.Events;
 
 public static class LevelEvents
 {
+    public enum PauseActions { PAUSE, PLAY }
+
     public class ObjectiveCompletedEvent : UnityEvent<LevelObjective> { }
     public class LevelWonEvent : UnityEvent<TeamBase> { }
     public class LevelIsMultiplayerEvent : UnityEvent { }
+    public class PauseLevelEvent : UnityEvent<PauseActions> { }
 
     public static ObjectiveCompletedEvent objectiveCompleteEvent { get; } = new ObjectiveCompletedEvent();
     public static LevelWonEvent levelWonEvent { get; } = new LevelWonEvent();
     public static LevelIsMultiplayerEvent levelIsMultiplayerEvent { get; } = new LevelIsMultiplayerEvent();
+    public static PauseLevelEvent pauseLevelEvent { get; } = new PauseLevelEvent();
 }
---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public void ResumeGame()
+    {
+        if(isPaused)
+        {
+            Unpause();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!pauseMenu)
+        {
+            Debug.LogError("Pause menu gameobject not assiged to PauseMenuController!");
+            return;
+        }
+
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        LevelEvents.pauseLevelEvent.Invoke(LevelEvents.PauseActions.PAUSE);
+        isPaused = true;
+    }
+
+    void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        LevelEvents.pauseLevelEvent.Invoke(LevelEvents.PauseActions.PLAY);
+        isPaused = false;
+    }
+
+    [SerializeField]
+    GameObject pauseMenu = null;
+
+    bool isPaused = false;
+}
