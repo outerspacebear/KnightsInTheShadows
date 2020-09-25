58683b04eb8687e8d3113853fcb1f500b81b7f01
Viraj <virjune@gmail.com>
2020-09-25 15:41:27 +0100

    Added character action sounds

---
+++ Assets/Scripts/Characters/CharacterActionAudioPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CharacterActionAudioPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!moveSound || !attackSound || !deathSound)
+        {
+            Debug.LogError("One or more of the sounds have not been asigned to CharacterActionAudioPlayer! It will not function.");
+            return;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+
+        CharacterEvents.actionTakenEvent.AddListener(OnCharacterActionTaken);
+        CharacterEvents.characterDeathEvent.AddListener(OnCharacterDeath);
+    }
+
+    private void OnDestroy()
+    {
+        CharacterEvents.actionTakenEvent.RemoveListener(OnCharacterActionTaken);
+        CharacterEvents.characterDeathEvent.RemoveListener(OnCharacterDeath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCharacterActionTaken(CCharacter character, ECharacterAction action)
+    {
+        AudioClip clipToPlay = null;
+        switch(action)
+        {
+            case ECharacterAction.MOVE:
+                clipToPlay = moveSound;
+                break;
+            case ECharacterAction.ATTACK:
+                clipToPlay = attackSound;
+                break;
+            default:
+                Debug.LogWarning("This should never happen! Case for a character action not covered in switch!");
+                break;
+        }
+
+        PlayClip(clipToPlay);  
+    }
+
+    void OnCharacterDeath(CCharacter character)
+    {
+        PlayClip(deathSound);
+    }
+
+    void PlayClip(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip moveSound = null;
+    [SerializeField]
+    AudioClip attackSound = null;
+    [SerializeField]
+    AudioClip deathSound = null;
+}
