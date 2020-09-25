0ba429f25dcdfeb3379f4ff1fe71445d0ae00472
Viraj <virjune@gmail.com>
2020-09-25 14:59:24 +0100

    Added sounds to buttons

---
+++ Assets/Scripts/UI/MenuButtonsSoundPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuButtonsSoundPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        foreach(var button in buttons)
+        {
+            button.onClick.AddListener(OnButtonClicked);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var button in buttons)
+        {
+            button.onClick.RemoveListener(OnButtonClicked);
+        }
+    }
+
+    void OnButtonClicked()
+    {
+        audioSource.Play();
+    }
+
+    Button[] buttons;
+    AudioSource audioSource;
+}
