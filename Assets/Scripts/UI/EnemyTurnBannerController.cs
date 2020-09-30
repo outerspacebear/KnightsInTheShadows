e534ea8c7187189cb2beaf242949fcd654199900
Viraj <virjune@gmail.com>
2020-09-30 16:43:43 +0100

    Added enemy turn banner for singleplayer levels

---
+++ Assets/Scripts/UI/EnemyTurnBannerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurnBannerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        banner.SetActive(false);
+
+        TeamEvents.teamTurnStartedEvent.AddListener(OnTeamTurnStarted);
+    }
+
+    void OnTeamTurnStarted(TeamBase team)
+    {
+        if(team.GetType() == typeof(CAITeam))
+        {
+            banner.SetActive(true);
+        }
+        else
+        {
+            banner.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [SerializeField]
+    GameObject banner = null;
+}
